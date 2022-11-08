f33ec3aefc1d52b086c302263bcd249a5acad90c
Nilton Heck <nilton@chainengine.xyz>
2022-11-08 16:28:23 -0300

    update library calls

--- src/index.js
+++ src/index.js
@@ -1,6 +1,37 @@
+const axios = require('axios').default;
+
 module.exports = {
-    sendTransactions: () => {
-        alert('Send Transaction');
-        // ...
+    sendTransaction: ({
+        from, 
+        to,
+        provider
+    }) => {
+        try {
+            let { data } = await axios.post(
+              "https://gasbit-demo-backend.herokuapp.com/message",
+              { from, to }
+            );
+      
+            // Sign the transaction
+            let signature = await provider.request({
+              method: "eth_signTypedData_v4",
+              params: [from, JSON.stringify(data)],
+              from: from
+            });
+      
+            console.log("signature: " + signature);
+      
+            let execute = await axios.post(
+              "https://gasbit-demo-backend.herokuapp.com/execute",
+              {
+                message: data.message,
+                signature: signature
+              }
+            );
+      
+            console.log("execute: ", execute.data.tx.hash);
+          } catch (e) {
+            console.error(e);
+          }
     }
 }
