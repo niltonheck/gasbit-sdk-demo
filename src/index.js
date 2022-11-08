43e6de72a11533e4d8e5c3ec833ba46b21a816ef
Nilton Heck <nilton@chainengine.xyz>
2022-11-08 16:43:35 -0300

    add transaction sender

--- src/index.js
+++ src/index.js
@@ -1,37 +1,41 @@
 const axios = require('axios').default;
 
-module.exports = {
-    sendTransaction: async ({
+class TransactionSender {
+    async sendTransaction({
         from, 
         to,
-        provider
-    }) => {
+        provider,
+        method,
+        params,
+    }) {
         try {
             let { data } = await axios.post(
-              "https://gasbit-demo-backend.herokuapp.com/message",
-              { from, to }
+                "https://gasbit-demo-backend.herokuapp.com/message",
+                { from, to, method, params }
             );
-      
+        
             // Sign the transaction
             let signature = await provider.request({
-              method: "eth_signTypedData_v4",
-              params: [from, JSON.stringify(data)],
-              from: from
+                method: "eth_signTypedData_v4",
+                params: [from, JSON.stringify(data)],
+                from: from
             });
-      
-            console.log("signature: " + signature);
-      
+        
             let execute = await axios.post(
-              "https://gasbit-demo-backend.herokuapp.com/execute",
-              {
+                "https://gasbit-demo-backend.herokuapp.com/execute",
+                {
                 message: data.message,
                 signature: signature
-              }
+                }
             );
-      
-            console.log("execute: ", execute.data.tx.hash);
-          } catch (e) {
+        
+            return execute.data.tx.hash;
+        } catch (e) {
             console.error(e);
-          }
+        }
     }
+}
+
+module.exports = {
+    TransactionSender: new TransactionSender()
 }
